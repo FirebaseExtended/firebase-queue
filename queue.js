39a4b4263f7a7bdba6d40576c65eb6846f5e4906
Chris Raynor <chris@firebase.com>
2014-10-31 10:32:44 -0700

    moving reset job listener out from QueueWorker to Queue and removing hacks for client bug

--- queue.js
+++ queue.js
@@ -5,7 +5,7 @@ var QueueWorker = require('./lib/queue_worker'),
  * @constructor
  * @param {String} referenceUrl The URL to the Firebase queue.
  * @param {String} token A JWT with the uid set to the current job ID.
- * @param {Integer} workers The number of workers to create for this job.
+ * @param {Integer} numWorkers The number of workers to create for this job.
  * @param {Function} processingFunction A function that is called each time to
  *   process the queue item. This function is passed three parameters:
  *     - data {Object} The current data at the location.
@@ -20,11 +20,11 @@ var QueueWorker = require('./lib/queue_worker'),
  */
 module.exports = Queue;
 
-function Queue(referenceUrl, token, workers, processingFunction) {
+function Queue(referenceUrl, token, numWorkers, processingFunction) {
   var self = this;
-  if (typeof(workers) !== 'number' ||
-        workers % 1 !== 0 ||
-        workers <= 0) {
+  if (typeof(numWorkers) !== 'number' ||
+        numWorkers % 1 !== 0 ||
+        numWorkers <= 0) {
     throw new Error('The number of workers must be a possitive integer');
   }
   self.ref = new Firebase(referenceUrl, new Firebase.Context());
@@ -33,9 +33,40 @@ function Queue(referenceUrl, token, workers, processingFunction) {
     if (error) {
       throw error;
     }
-    for (var i = 0; i < workers; i++) {
+    for (var i = 0; i < numWorkers; i++) {
       self.workers.push(QueueWorker(self.ref, i, processingFunction));
     }
+
+    self.ref.parent().child('_jobs').child(self.jobId).on('value',
+      function(jobSpecSnap) {
+        if (jobSpecSnap.val() === null) {
+          throw new Error('No job specified for this worker');
+        }
+        if (jobSpecSnap.child('state').val() === null) {
+          throw new Error('No state specified for this job');
+        }
+
+        var jobSpec = {
+          startState: jobSpecSnap.child('state/start').val(),
+          inProgressState: jobSpecSnap.child('state/inProgress').val(),
+          finishedState: jobSpecSnap.child('state/finished').val(),
+          jobTimeout: jobSpecSnap.child('timeout').val()
+        };
+
+        if (!jobSpec.inProgressState) {
+          throw new Error('No inProgress state specified for this job');
+        }
+        if (!jobSpec.finishedState) {
+          throw new Error('No finished state specified for this job');
+        }
+
+        for (var i = 0; i < numWorkers; i++) {
+          self.workers[i].resetJob(jobSpec);
+        }
+      },
+      function(error) {
+        throw error;
+      });
   });
   return self;
 }
