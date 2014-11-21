127dbd86474a9086c983aa79a2351ea02ad96195
Chris Raynor <chris@firebase.com>
2014-11-20 16:34:27 -0800

    Changing interface to pass in the Firebase reference and jobID instead of the URL and token

--- queue.js
+++ queue.js
@@ -1,10 +1,9 @@
-var QueueWorker = require('./lib/queue_worker'),
-    Firebase = require('firebase');
+var QueueWorker = require('./lib/queue_worker');
 
 /**
  * @constructor
- * @param {String} referenceUrl The URL to the Firebase queue.
- * @param {String} token A JWT with the uid set to the current job ID.
+ * @param {Firebase} ref A firebase reference to the queue.
+ * @param {String} jobId the current job ID.
  * @param {Integer} numWorkers The number of workers to create for this job.
  * @param {Function} processingFunction A function that is called each time to
  *   process the queue item. This function is passed three parameters:
@@ -20,55 +19,49 @@ var QueueWorker = require('./lib/queue_worker'),
  */
 module.exports = Queue;
 
-function Queue(referenceUrl, token, numWorkers, processingFunction) {
+function Queue(ref, jobId, numWorkers, processingFunction) {
   var self = this;
   if (typeof(numWorkers) !== 'number' ||
         numWorkers % 1 !== 0 ||
         numWorkers <= 0) {
     throw new Error('The number of workers must be a possitive integer');
   }
-  self.ref = new Firebase(referenceUrl, new Firebase.Context());
+  self.ref = ref;
   self.workers = [];
-  self.ref.authWithCustomToken(token, function(error, authData) {
-    if (error) {
+  for (var i = 0; i < numWorkers; i++) {
+    self.workers.push(QueueWorker(self.ref, i, processingFunction));
+  }
+
+  self.ref.parent().child('_jobs').child(jobId).on('value',
+    function(jobSpecSnap) {
+      if (jobSpecSnap.val() === null) {
+        throw new Error('No job specified for this worker');
+      }
+      if (jobSpecSnap.child('state').val() === null) {
+        throw new Error('No state specified for this job');
+      }
+
+      var jobSpec = {
+        startState: jobSpecSnap.child('state/start').val(),
+        inProgressState: jobSpecSnap.child('state/inProgress').val(),
+        finishedState: jobSpecSnap.child('state/finished').val(),
+        jobTimeout: jobSpecSnap.child('timeout').val()
+      };
+
+      if (!jobSpec.inProgressState) {
+        throw new Error('No inProgress state specified for this job');
+      }
+      if (!jobSpec.finishedState) {
+        throw new Error('No finished state specified for this job');
+      }
+
+      for (var i = 0; i < numWorkers; i++) {
+        self.workers[i].resetJob(jobSpec);
+      }
+    },
+    function(error) {
       throw error;
-    }
-    for (var i = 0; i < numWorkers; i++) {
-      self.workers.push(QueueWorker(self.ref, i, processingFunction));
-    }
+    });
 
-    var jobId = authData.uid;
-
-    self.ref.parent().child('_jobs').child(jobId).on('value',
-      function(jobSpecSnap) {
-        if (jobSpecSnap.val() === null) {
-          throw new Error('No job specified for this worker');
-        }
-        if (jobSpecSnap.child('state').val() === null) {
-          throw new Error('No state specified for this job');
-        }
-
-        var jobSpec = {
-          startState: jobSpecSnap.child('state/start').val(),
-          inProgressState: jobSpecSnap.child('state/inProgress').val(),
-          finishedState: jobSpecSnap.child('state/finished').val(),
-          jobTimeout: jobSpecSnap.child('timeout').val()
-        };
-
-        if (!jobSpec.inProgressState) {
-          throw new Error('No inProgress state specified for this job');
-        }
-        if (!jobSpec.finishedState) {
-          throw new Error('No finished state specified for this job');
-        }
-
-        for (var i = 0; i < numWorkers; i++) {
-          self.workers[i].resetJob(jobSpec);
-        }
-      },
-      function(error) {
-        throw error;
-      });
-  });
   return self;
 }
