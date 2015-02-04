15bb040a84a9e9613e9464ab11f4854b90c292f9
Matthew Tse <mtse@google.com>
2015-02-03 16:35:35 -0800

    changes

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ A fault-tolerant, multi-worker, multi-stage job pipeline based on Firebase.
 ## The Queue
 
 The queue itself will be a location in a Firebase of your choosing that the
-items will be pushed to. e.g. `https://yourapp.firebaseio.com/`
+items will be pushed to. e.g. `https://yourapp.firebaseio.com/queue`
 
 ## Defining Jobs
 
@@ -18,32 +18,25 @@ your queue location. e.g. assuming the queue location of
 ```json
 {
   "job_1" : {
-    "state" : {
-      "finished" : "job_1_finished",
-      "inProgress" : "job_1_in_progress",
-      "start" : "job_1_ready"
-    },
+    "state_finished" : "job_1_finished",
+    "state_in_progress" : "job_1_in_progress",
+    "state_start" : "job_1_ready",
     "timeout" : 5000
   },
   "job_2" : {
-    "state" : {
-      "finished" : "job_2_finished",
-      "inProgress" : "job_2_in_progress",
-      "start" : "job_1_finished"
-    },
+    "state_finished" : "job_2_finished",
+    "state_in_progress" : "job_2_in_progress",
+    "state_start" : "job_1_finished",
     "timeout" : 9000
   }
 }
 ```
 
-`state/inProgress` and `state/finished` are required for each job, but
-`state/start` and `timeout` are optional. If the start state of the job is
+`state_in_progress` and `state_finished` are required for each job, but
+`state_start` and `timeout` are optional. If the start state of the job is
 omitted, the workers will match any item in the list without a `_state`
 parameter, and if `timeout` is omitted, each job will only ever be attempted
 once.
-
-Currently omitting `state/start` is broken, but a fix to the client should be
-coming soon.
 
 Here we're chaining jobs by specifying the same `state/start` for `job_2` as
 `state/finished` for `job_1`. If any job errors, the `_state` parameter is set
@@ -84,5 +77,5 @@ ensure that only one worker is processing a single queue item at a time.
 
 The final stage is pushing items onto the queue. Because we're using Firebase
 this is the easy bit. From any Firebase client, simply push an object with some
-metadata to pass to the first job to the queue location, with the `_state` key
-matching the `state/start` of the first job.
+metadata to pass to the first job to the queue location, optionally setting the
+ `_state` key matching the `state_start` of the first job.
--- queue.js
+++ queue.js
@@ -3,8 +3,9 @@ var QueueWorker = require('./lib/queue_worker');
 /**
  * @constructor
  * @param {Firebase} ref A firebase reference to the queue.
- * @param {String} jobId the current job ID.
- * @param {Integer} numWorkers The number of workers to create for this job.
+ * @param {Object} (optional) 
+     Object containing keys for JobID: the current job ID.
+                            and numWorkers: The number of workers to create for this job.
  * @param {Function} processingFunction A function that is called each time to
  *   process the queue item. This function is passed three parameters:
  *     - data {Object} The current data at the location.
@@ -19,51 +20,92 @@ var QueueWorker = require('./lib/queue_worker');
  */
 module.exports = Queue;
 
-function Queue(ref, jobId, numWorkers, processingFunction) {
-  var self = this;
-  if (typeof(numWorkers) !== 'number' ||
-        numWorkers % 1 !== 0 ||
-        numWorkers <= 0) {
-    throw new Error('The number of workers must be a positive integer');
+var DEFAULT_JOB_ID = "Default_Job_ID";
+var DEFAULT_JOB_STATE_FINISHED = "Job Finished";
+var DEFAULT_JOB_STATE_IN_PROGRESS = "Job In Progress";
+var DEFAULT_NUM_WORKERS = 1;
+var DEFAULT_TIMEOUT = 360000;
+
+function Queue() {
+  if (arguments.length == 2) {
+    ref = arguments[0];
+    jobID = DEFAULT_JOB_ID;
+    numWorkers = DEFAULT_NUM_WORKERS;
+    processingFunction = arguments[1];
   }
+  else if (arguments.length == 3) {
+    ref = arguments[0];
+    options = arguments[1];
+    if ('jobID' in options) {
+      jobID = options.jobID;
+    }
+    else {
+      jobID = DEFAULT_JOB_ID;
+    }
+    if ('numWorkers' in options) {
+      numWorkers = options.numWorkers;
+      if (typeof(numWorkers) !== 'number' ||
+            numWorkers % 1 !== 0 ||
+            numWorkers <= 0) {
+        throw new Error('The number of workers must be a positive integer');
+      }
+    }
+    else {
+      numWorkers = DEFAULT_NUM_WORKERS;
+    }
+    processingFunction = arguments[2];
+  }
+
+  var self = this;
+
   self.ref = ref;
   self.workers = [];
   for (var i = 0; i < numWorkers; i++) {
     self.workers.push(QueueWorker(self.ref.child('queue'), i, processingFunction));
   }
 
-  self.ref.child('jobs').child(jobId).on('value',
-    function(jobSpecSnap) {
-      if (jobSpecSnap.val() === null) {
-        throw new Error('No job specified for this worker');
-      }
-      if (jobSpecSnap.child('state').val() === null) {
-        throw new Error('No state specified for this job');
-      }
+  if (jobID !== DEFAULT_JOB_ID) {
+    self.ref.child('jobs').child(jobID).on('value',
+      function(jobSpecSnap) {
+        if (jobSpecSnap.val() === null) {
+          throw new Error('No job specified for this worker');
+        }
+        finishedState = jobSpecSnap.child('state_finished').val();
+        if (finishedState === null) {
+          throw new Error('No state_finished specified for this job');
+        }
+        inProgressState = jobSpecSnap.child('state_in_progress').val();
+        if (inProgressState === null) {
+          throw new Error('No state_in_progress specified for this job');
+        }
 
-      // TODO: change state specification to be more intuative
+        var jobSpec = {
+          startState: jobSpecSnap.child('state_start').val(),
+          inProgressState: inProgressState,
+          finishedState: finishedState,
+          jobTimeout: jobSpecSnap.child('timeout').val()
+        };
 
-      var jobSpec = {
-        startState: jobSpecSnap.child('state/start').val(),
-        inProgressState: jobSpecSnap.child('state/inProgress').val(),
-        finishedState: jobSpecSnap.child('state/finished').val(),
-        jobTimeout: jobSpecSnap.child('timeout').val()
-      };
-
-      if (!jobSpec.inProgressState) {
-        throw new Error('No inProgress state specified for this job');
-      }
-      if (!jobSpec.finishedState) {
-        throw new Error('No finished state specified for this job');
-      }
-
-      for (var i = 0; i < numWorkers; i++) {
-        self.workers[i].resetJob(jobSpec);
-      }
-    },
-    function(error) {
-      throw error;
-    });
+        for (var i = 0; i < numWorkers; i++) {
+          self.workers[i].resetJob(jobSpec);
+        }
+      },
+      function(error) {
+        throw error;
+      });
+    
+  }
+  else {
+    jobSpec = {
+      startState: null,
+      inProgressState: DEFAULT_JOB_STATE_IN_PROGRESS,
+      finishedState: DEFAULT_JOB_STATE_FINISHED,
+      jobTimeout: DEFAULT_TIMEOUT
+    };
+    for (var j = 0; j < numWorkers; j++) {
+      self.workers[j].resetJob(jobSpec);
+    }
+  }
 
   return self;
 }
