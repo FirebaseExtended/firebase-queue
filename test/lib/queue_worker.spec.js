cb26e4e0f4743c8641166c40302503a8f0973705
Paul des Garets <paul.des-garets@student.ecp.fr>
2016-03-21 12:40:42 -0700

    Give access to task ID to the worker

--- test/lib/queue_worker.spec.js
+++ test/lib/queue_worker.spec.js
@@ -1138,7 +1138,7 @@ describe('QueueWorker', function() {
                 try {
                   testRef.off();
                   var task = snapshot.val();
-                  expect(task).to.have.all.keys(['_state', '_progress', '_state_changed', '_error_details']);
+                  expect(task).to.have.all.keys(['_state', '_progress', '_state_changed', '_error_details', '_id']);
                   expect(task['_state']).to.equal('error');
                   expect(task['_state_changed']).to.be.closeTo(new Date().getTime() + th.offset, 250);
                   expect(task['_progress']).to.equal(0);
@@ -1314,7 +1314,7 @@ describe('QueueWorker', function() {
     it('should not sanitize data passed to the processing function when specified', function(done) {
       qw = new th.QueueWorker(tasksRef, '0', false, false, function(data, progress, resolve, reject) {
         try {
-          expect(data).to.have.all.keys(['foo', '_owner', '_progress', '_state', '_state_changed']);
+          expect(data).to.have.all.keys(['foo', '_owner', '_progress', '_state', '_state_changed', '_id']);
           done();
         } catch (error) {
           done(error);
