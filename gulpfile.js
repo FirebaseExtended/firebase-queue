96cdd0198a312b0ff1fd4b09eaf3257a7a46add0
Chris Raynor <raynor@google.com>
2016-04-13 18:23:22 -0700

    Switching Queue to eslint 2, and cleaning up the repo a little

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,22 @@
-/**************/
-/*  REQUIRES  */
-/**************/
+'use strict';
+
+/* ************ */
+/*   REQUIRES   */
+/* ************ */
 var gulp = require('gulp');
 
 // File I/O
 var exit = require('gulp-exit');
-var jshint = require('gulp-jshint');
 
 // Testing
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
+var eslint = require('gulp-eslint');
 
 
-/****************/
-/*  FILE PATHS  */
-/****************/
+/* ************** */
+/*   FILE PATHS   */
+/* ************** */
 var paths = {
   js: {
     srcFiles: [
@@ -30,18 +32,15 @@ var paths = {
 };
 
 
-/***********/
-/*  TASKS  */
-/***********/
+/* ********* */
+/*   TASKS   */
+/* ********* */
 // Lints the JavaScript files and copies them to the destination directory
 gulp.task('build', function() {
   return gulp.src(paths.js.srcFiles)
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(jshint.reporter('fail'))
-    .on('error', function(error) {
-      throw error;
-    })
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
     .pipe(gulp.dest(paths.js.destDir));
 });
 
@@ -50,11 +49,11 @@ gulp.task('test', function() {
   return gulp.src(paths.js.srcFiles)
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
-    .on('finish', function () {
+    .on('finish', function() {
       gulp.src(paths.tests)
         .pipe(mocha({
           reporter: 'spec',
-          timeout: 5000
+          timeout: 10000
         }))
         .pipe(istanbul.writeReports())
         .pipe(exit());
